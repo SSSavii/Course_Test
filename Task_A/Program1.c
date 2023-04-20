c5ecc771a2b48f2f171d13d8eff4e1ea9ae99507
SSSavii <georgiii3512@mail.ru>
2023-04-20 11:43:25 +0700

    correct Task A

--- Task_A/Program1.c
+++ Task_A/Program1.c
@@ -2,17 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <Windows.h>
 int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    int N;
     char Str[100000];
     char last_open = ' ';
+    int n, N;
     scanf("%d", &N);
+    getchar();
     gets(Str);
+    n = strlen(Str);
+    if (N > 100000 || N < 1)
+    {
+        printf("The entered number does not fall within the specified boundaries(1<=N<=100000)");
+        return -1;
+    }
+    if (n != N)
+    {
+        printf("The entered number differs from the number of brackets");
+        return -1;
+    }
     int round = 0, figured = 0, squred = 0;
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < n; i++)
     {
         if (Str[i] == '(')
             round++;
@@ -30,14 +39,18 @@ int main() {
         if (Str[i] == '(' || Str[i] == '{' || Str[i] == '[')
         {
             last_open = Str[i];
-
+        }
+        if (Str[i] != '(' && Str[i] != ')' && Str[i] != '{' && Str[i] != '}' && Str[i] != '[' && Str[i] != ']')
+        {
+            printf("an invalid element is present in the entered line");
+            return -1;
         }
         if ((Str[i] == ')' && last_open != '(') || (Str[i] == '}' && last_open != '{') || (Str[i] == ']' && last_open != '['))
             break;
     }
     if (round || figured || squred)
-        return false;
+        printf("NO");
     else
-        return true;
+        printf("Yes");
     return 0;
 }
